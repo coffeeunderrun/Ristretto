820ede06813eb1612989a703854e72219ea1fe20
CoffeeUnderrun <jamie@coffeeunderrun.dev>
2026-01-31 13:46:27 -0500

    Pass RSDP address from Limine response to uACPI.
    - Move kernel specific uACPI procs to kernel where they belong.
    - Get RSDP address and provide it to uACPI via 'uacpi_kernel_get_rsdp'.
    - Pass uACPI log messages to terminal via 'uacpi_kernel_log'.
    - Add supporting kernel RTL procs from FPC's RTL.

--- kernel/kernel.pas
+++ kernel/kernel.pas
@@ -1,6 +1,6 @@
 program Kernel;
 
-uses Arch, Limine, Framebuffer, Terminal, Uacpi;
+uses Arch, Limine, Log, Framebuffer, Terminal, Uacpi;
 
 {$define LIMINE_REQUEST_FRAMEBUFFER}
 {$define LIMINE_REQUEST_RSDP}
@@ -14,13 +14,44 @@ const
     '|___|__|__|_____|____|__| |_____|____|____|_____|'#10#10;
 
 var
+  Rsdp: TLimineRsdpRequest; external name '_limine_request_rsdp';
   Buffer: array [0..4095] of Byte;
   Status: UacpiStatus;
+
+function KernelGetRsdp(AddressPtr: PUacpiPhysAddr): UacpiStatus; cdecl; public name 'uacpi_kernel_get_rsdp';
+begin
+  if Rsdp.Response <> nil then begin
+    AddressPtr^ := UacpiPhysAddr(Rsdp.Response^.Address);
+    exit(UACPI_STATUS_OK);
+  end;
+
+  result := UACPI_STATUS_NOT_FOUND;
+end;
+
+procedure KernelLog(LogLevel: UacpiLogLevel; const Message: PUacpiChar); cdecl; public name 'uacpi_kernel_log';
+begin
+  case LogLevel of
+    UACPI_LOG_ERROR: Log.Error(Message);
+    UACPI_LOG_WARN:  Log.Warn (Message);
+    UACPI_LOG_INFO:  Log.Info (Message);
+    UACPI_LOG_TRACE: Log.Trace(Message);
+    UACPI_LOG_DEBUG: Log.Debug(Message);
+  end;
+end;
+
+function KernelMap(Address: UacpiPhysAddr; Size: UacpiSize): Pointer; cdecl; public name 'uacpi_kernel_map';
+begin
+  result := nil;
+end;
+
+procedure KernelUnmap(Ptr: Pointer; Size: UacpiSize); cdecl; public name 'uacpi_kernel_unmap';
+begin
+end;
 
 begin
   if not Limine.BaseRevisionSupported then exit;
 
   Terminal.Write(Logo);
 
-  Status := UacpiSetupEarlyTableAccess(@Buffer, SizeOf(Buffer));
+  Status := Uacpi.SetupEarlyTableAccess(@Buffer, SizeOf(Buffer));
 end.
