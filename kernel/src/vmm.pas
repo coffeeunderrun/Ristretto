249aad8c02b4204f6d0cef2479afded6dabc0711
CoffeeUnderrun <jamie@coffeeunderrun.dev>
2026-02-11 15:31:45 -0500

    Map kernel segments with the appropriate access flags.

--- kernel/src/vmm.pas
+++ kernel/src/vmm.pas
@@ -8,52 +8,65 @@ implementation
 
 uses ArchApi, Framebuffer, Limine, Log, Pmm, SysUtils, Utilities;
 
+const
+  KernelAccessExecutable = [MemoryAccessGlobal, MemoryAccessExecute];
+  KernelAccessWritable = [MemoryAccessGlobal, MemoryAccessWrite];
+  KernelAccessReadOnly = [MemoryAccessGlobal];
+
 var
   ExecutableAddressRequest: TLimineExecutableAddressRequest; external name '_limine_request_executable_address';
   MemoryMapRequest: TLimineMemoryMapRequest; external name '_limine_request_memory_map';
-  KernelStart: Pointer; external name '_kernel_start';
   KernelEnd: Pointer; external name '_kernel_end';
+  KernelTextStart: Pointer; external name '_kernel_text_start';
+  KernelTextEnd: Pointer; external name '_kernel_text_end';
+  KernelRodataStart: Pointer; external name '_kernel_rodata_start';
+  KernelRodataEnd: Pointer; external name '_kernel_rodata_end';
 
 function CreateKernelAddressSpace: PtrUInt;
 var
-  RootFrame: PtrUInt;
+  RootFrame, KernelFrame, KernelPage: PtrUInt;
   EntryIndex: SizeUInt;
-  MemoryCache: TMemoryCache;
+  MapSuccess: Boolean;
 begin
   RootFrame := 0;
+
   if not CreateRootFrame(RootFrame, @EarlyAllocateFrame, @AddHhdmOffset) then
     Panic('Failed to create kernel address space root frame.');
 
-  { TODO: Use proper flags based on segments. }
-  // Map the kernel executable region.
-  if not MapPageRange(
-    RootFrame,
-    ExecutableAddressRequest.Response^.PhysicalBase,
-    ExecutableAddressRequest.Response^.VirtualBase,
-    PtrUInt(@KernelEnd) - PtrUInt(@KernelStart),
-    [MemoryAccessGlobal, MemoryAccessExecute, MemoryAccessWrite],
-    MemoryCacheWriteBack,
-    @EarlyAllocateFrame, @AddHhdmOffset
-  ) then Panic('Failed to map kernel executable region.');
+  // Map the kernel region.
+  KernelFrame := ExecutableAddressRequest.Response^.PhysicalBase;
+  KernelPage := ExecutableAddressRequest.Response^.VirtualBase;
+  while KernelPage < PtrUInt(@KernelEnd) do begin
+    // Map kernel text as executable.
+    if (KernelPage >= PtrUInt(@KernelTextStart)) and (KernelPage < PtrUInt(@KernelTextEnd)) then
+      MapSuccess := MapPage(RootFrame, KernelFrame, KernelPage,
+        KernelAccessExecutable, MemoryCacheWriteBack, @EarlyAllocateFrame, @AddHhdmOffset)
+    // Map kernel rodata as read-only.
+    else if (KernelPage >= PtrUInt(@KernelRodataStart)) and (KernelPage < PtrUInt(@KernelRodataEnd)) then
+      MapSuccess := MapPage(RootFrame, KernelFrame, KernelPage,
+        KernelAccessReadOnly, MemoryCacheWriteBack, @EarlyAllocateFrame, @AddHhdmOffset)
+    // Map the rest of the kernel as writable.
+    else
+      MapSuccess := MapPage(RootFrame, KernelFrame, KernelPage,
+      KernelAccessWritable, MemoryCacheWriteBack, @EarlyAllocateFrame, @AddHhdmOffset);
 
-  // Use higher-half direct mapping to map physical memory.
+    if not MapSuccess then Panic('Failed to map kernel.');
+
+    Inc(KernelFrame, PageSize);
+    Inc(KernelPage, PageSize);
+  end;
+
+  // Use higher-half direct mapping.
   with MemoryMapRequest.Response^ do
     for EntryIndex := 0 to EntryCount - 1 do with Entries^[EntryIndex] do begin
       if EntryType = LIMINE_MEMMAP_FRAMEBUFFER then
-        MemoryCache := MemoryCacheWriteCombining
+        MapSuccess := MapPageRange(RootFrame, Base, AddHhdmOffset(Base), Length,
+        KernelAccessWritable, MemoryCacheWriteCombining, @EarlyAllocateFrame, @AddHhdmOffset)
       else
-        MemoryCache := MemoryCacheWriteBack;
+        MapSuccess := MapPageRange(RootFrame, Base, AddHhdmOffset(Base), Length,
+        KernelAccessWritable, MemoryCacheWriteBack, @EarlyAllocateFrame, @AddHhdmOffset);
 
-      if not MapPageRange(
-        RootFrame,
-        Base,
-        AddHhdmOffset(Base),
-        Length,
-        [MemoryAccessGlobal, MemoryAccessWrite],
-        MemoryCache,
-        @EarlyAllocateFrame,
-        @AddHhdmOffset
-      ) then Panic('Failed to map physical memory range.');
+      if not MapSuccess then Panic('Failed to map HHDM.');
     end;
 
   result := RootFrame;
